098fecbf01cbdfa8bed9eaa4269df575dfcf8f17
Nelson Modenez Neto <nelsonmodenezneto@hotmail.com>
2023-09-20 06:51:37 -0300

    tt
    yy

--- Portifolio/js.js
+++ Portifolio/js.js
@@ -1,28 +1,81 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var navbar = document.getElementById("navbar");
-    var originalBackgroundColor = getComputedStyle(navbar).backgroundColor;
+  var navbar = document.getElementById("navbar");
+  var originalBackgroundColor = getComputedStyle(navbar).backgroundColor;
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 10) {
-            
-            navbar.classList.add("navbar-dark");
-            navbar.style.backgroundColor = "black";
-            imagemClara.style.display = "none";
-            imagemEscura.style.display = "block";
-        } else {
-            navbar.classList.remove("navbar-dark");
-            navbar.style.backgroundColor = originalBackgroundColor;
-            imagemClara.style.display = "block";
-            imagemEscura.style.display = "none";
-        }
-    });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 10) {
+
+      navbar.classList.add("navbar-dark");
+      navbar.style.backgroundColor = "black";
+      imagemClara.style.display = "none";
+      imagemEscura.style.display = "block";
+    } else {
+      navbar.classList.remove("navbar-dark");
+      navbar.style.backgroundColor = originalBackgroundColor;
+      imagemClara.style.display = "block";
+      imagemEscura.style.display = "none";
+    }
+  });
 });
 
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Função para verificar todos os cards e animá-los se estiverem na visão do usuário
+function checkCardsVisibility() {
+  var minhahistoria = document.querySelectorAll(".card");
+  minhahistoria.forEach(function (card) {
+    if (isElementInViewport(card)) {
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+  });
+}
+
+window.addEventListener("scroll", checkCardsVisibility);
+window.addEventListener("load", checkCardsVisibility);
+
+
+function checkHistoriaVisibility() {
+  var minhahistoria = document.querySelectorAll(".opacity");
+  minhahistoria.forEach(function (card) {
+    if (isElementInViewport(card)) {
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+  });
+}
+
+
+window.addEventListener("scroll", checkHistoriaVisibility);
+window.addEventListener("load", checkHistoriaVisibility);
+
+function checkEfeitoVisibility() {
+  var efeito = document.querySelectorAll(".efeito");
+  efeito.forEach(function (card) {
+    if (isElementInViewport(card)) {
+      card.style.opacity = "1";
+      card.style.transform = "translateX(0)";
+    }
+  });
+}
+
+window.addEventListener("scroll", checkEfeitoVisibility);
+window.addEventListener("load", checkEfeitoVisibility);
+
+
 document.addEventListener('DOMContentLoaded', function () {
-    var carousel = document.getElementById('palavrasCarousel');
-    var carouselInstance = new bootstrap.Carousel(carousel, {
-        interval: 2000 // Defina o intervalo de tempo entre as palavras em milissegundos
-    });
+  var carousel = document.getElementById('palavrasCarousel');
+  var carouselInstance = new bootstrap.Carousel(carousel, {
+    interval: 2000 // Defina o intervalo de tempo entre as palavras em milissegundos
+  });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
